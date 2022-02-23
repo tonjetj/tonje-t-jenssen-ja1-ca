bed557066d49936d34884279188515f38ea46982
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-02-23 16:45:59 +0100

    added styling to navbar and homepage
    not yet finished with styling on cards showing on the homepage

--- js/script.js
+++ js/script.js
@@ -19,12 +19,17 @@ async function fetchCards() {
     resultsContainer.innerHTML = "";
 
     for (let i = 0; i < cards.length; i++) {
-      if (!cards[i].img === true) {
-        cards[i].name = "";
+      if (i === 60) {
+        break;
       }
 
-      resultsContainer.innerHTML += ` <a href="details.html?id=" class="card"><img src="${cards[i].img}" alt="${cards[i].name}"/>
-      <h2>${cards[i].name}<h2></div></a>`;
+      resultsContainer.innerHTML += `<a href="details.html?id=${cards[i].id}" class="card">
+                                        <div class="image" style="background-image: url(${cards[i].img});"></div>
+                                              <div class="details">
+                                                  <h2 class="name">${cards[i].name}</h2>
+                                              </div>
+                                        </a>
+      `;
     }
   } catch (error) {
     resultsContainer.innerHTML = message("error", error);
@@ -32,3 +37,23 @@ async function fetchCards() {
 }
 
 fetchCards();
+
+const nav = document.querySelector("nav");
+
+function handleScroll() {
+  console.log(window.scrollY);
+
+  const scrolledY = window.scrollY;
+
+  if (scrolledY > 10) {
+    document.body.classList.add("scrolled");
+    nav.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+    nav.classList.remove("scrolled");
+  }
+}
+
+window.onscroll = handleScroll;
+
+window.addEventListener("scroll", handleScroll);
